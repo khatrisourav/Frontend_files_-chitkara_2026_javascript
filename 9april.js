9e7d47d63171414e6cf560a3ef5fb2a49a191a2d
khatrisourav <sourabhbatra403@gmail.com>
2026-04-09 17:03:49 +0530

    new

---
+++ 9april.js
@@ -0,0 +1,39 @@
+// // // let evt = {
+// // //     x: 23,
+// // //     y :29,
+// // // }
+// // // console.log(evt.x, evt.y)
+
+
+// // // function n (event){
+// // //     console.log(event)
+// // // }
+// // // n(evt)
+
+
+// // document.getElementById('btn1').addEventListener('click', function() {
+// //     // console.log(event.x, event.y)
+// //     console.log("child")
+// // },true)
+
+// // document.getElementById('box').addEventListener('click', function() {
+// //     // console.log(event.x, event.y)
+// //     console.log("parent")
+// // },true)
+
+// // document.getElementById('box1').addEventListener('click', function(){
+// //     console.log('body')
+// // },true)
+
+
+// // Select the parent element
+// const menu = document.getElementById('menu');
+
+// // Attach event listener to parent
+// menu.addEventListener('click', function (event) {
+//     // event.target is the actual element clicked
+
+
+//     console.log(event)
+
+// });
