5d65e73e20fe098be161b42a794f9365ae3eee23
khatrisourav <sourabhbatra403@gmail.com>
2026-04-10 19:11:49 +0530

    10april commit

---
+++ 10april.js
@@ -0,0 +1,29 @@
+// /*document.getElementById('heading').innerText= "Bye bye from our university"
+// document.getElementById('heading').innerText= "Bye bye from our university"
+// document.getElementById('heading').innerText= "Bye bye from our university"
+// document.getElementById('heading').innerText= "Bye bye from our university"
+// document.getElementById('heading').innerText= "Bye bye from our university"
+// */
+
+// document.getElementById('btn1').addEventListener('click', function(event){
+//    event.stopImmediatePropagation();
+//     console.log("child")
+// })
+
+
+// document.getElementById('btn1').addEventListener('click', function(event){
+  
+//     console.log("child1")
+// })
+
+
+// document.getElementById('btn1').addEventListener('click', function(event){
+  
+//     console.log("child2")
+// })
+// // document.getElementById('box').addEventListener('click', function(){
+// //     console.log("parent")
+// // })
+
+
+document.getElementById('heading').innerText= "Hello world"
