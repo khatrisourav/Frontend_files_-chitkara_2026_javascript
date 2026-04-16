ef00508432e0d8c82be6c1d1b9a93179cfad157f
khatrisourav <sourabhbatra403@gmail.com>
2026-04-16 17:20:29 +0530

    16april

--- 10april.js
+++ 10april.js
@@ -1,29 +1,43 @@
-// /*document.getElementById('heading').innerText= "Bye bye from our university"
-// document.getElementById('heading').innerText= "Bye bye from our university"
-// document.getElementById('heading').innerText= "Bye bye from our university"
-// document.getElementById('heading').innerText= "Bye bye from our university"
-// document.getElementById('heading').innerText= "Bye bye from our university"
-// */
+// // /*document.getElementById('heading').innerText= "Bye bye from our university"
+// // document.getElementById('heading').innerText= "Bye bye from our university"
+// // document.getElementById('heading').innerText= "Bye bye from our university"
+// // document.getElementById('heading').innerText= "Bye bye from our university"
+// // document.getElementById('heading').innerText= "Bye bye from our university"
+// // */
 
-// document.getElementById('btn1').addEventListener('click', function(event){
-//    event.stopImmediatePropagation();
-//     console.log("child")
-// })
-
-
-// document.getElementById('btn1').addEventListener('click', function(event){
-  
-//     console.log("child1")
-// })
-
-
-// document.getElementById('btn1').addEventListener('click', function(event){
-  
-//     console.log("child2")
-// })
-// // document.getElementById('box').addEventListener('click', function(){
-// //     console.log("parent")
+// // document.getElementById('btn1').addEventListener('click', function(event){
+// //    event.stopImmediatePropagation();
+// //     console.log("child")
 // // })
 
 
-document.getElementById('heading').innerText= "Hello world"
+// // document.getElementById('btn1').addEventListener('click', function(event){
+  
+// //     console.log("child1")
+// // })
+
+
+// // document.getElementById('btn1').addEventListener('click', function(event){
+  
+// //     console.log("child2")
+// // })
+// // // document.getElementById('box').addEventListener('click', function(){
+// // //     console.log("parent")
+// // // })
+
+
+// document.getElementById('heading').innerText= "Hello world"
+
+
+
+
+
+
+
+
+// document.getElementById('box').addEventListener('click', function() {
+//     console.log("parent")
+// })
+
+
+document.getElementById('heading').innerText= "bye from university"
